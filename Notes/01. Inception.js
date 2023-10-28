552b28d01da09231a3a51b546cc01eb2519a16bb
Suraj <mouryasuraj6266444@gmail.com>
2023-10-28 23:56:44 +0530

    initial

---
+++ Notes/01. Inception.js
@@ -0,0 +1,20 @@
+// Notes
+
+/*
+
+1st way to use react -- Using CDN react
+    Put the below code inside body tag at the end
+            <script crossorigin src="https://unpkg.com/react@18/umd/react.development.js"></script>
+            <script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
+    1. The first script is a core react which is written by facebook engineer using js because at the end react is a js library
+    2. The second script is a react-dom by using this we can manipulate DOM
+    3. React made different libraries for  react-dom(browser), react-native(mobile), react 3d,
+
+*/
+
+
+// Progress of leacture
+// Inception - resume from 30:00
+
+
+
