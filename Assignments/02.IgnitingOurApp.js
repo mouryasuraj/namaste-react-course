de88ccdb137a5018c0a9d2f4a5931ea7c4927524
Suraj <mouryasuraj6266444@gmail.com>
2023-11-15 11:31:38 +0530

    Completed the assignment of Ingiting out app

--- Assignments/02.IgnitingOurApp.js
+++ Assignments/02.IgnitingOurApp.js
@@ -68,4 +68,76 @@
      2. It is an feature that allows develpors to update module in a running application without requiring full page refresh.
      3. HMR significantly speeds up the development workflow by reducing the time it takes to see the effect of code changes. Developers can see result of their changes instantly, whithout refreshing the page.
      4. HMR is not limited to javaScript modules. It can also handle updates to stylesheets, images and other assets, making it a versatile tool for web development.
+
+8.List down your favourite 5 superpower of parcel and describe any 3 of them in your own words
+--->1.Zero Configuration
+    2.Tree Shaking
+    3.Code Spliting
+    4.Efficient asset handling
+    5.HMR
+
+    Describe any 3
+        1.Zero Configuration:
+                a.Parcel is designed to require minimal to no configuration. This means that we can start using parcel without spending time on complex configuration files.
+                b.The zero config approach reduce the barrier to entry and  makes it easy for developers to get started with building web applications.
+        2.Code Splitting:
+                a.Code splitting is a technique used in web development to split a larger javascript bundle into smaller, more manageable parts.
+                b.This can improve the performance of web application by loading only the necessary code for a particular page or feature, reducing the initial page load time and improve performance.
+        3.Efficient asset handling
+                a.Parcel has built in support for various asset types, including images, fonts, and CSS. It handles these assets efficiently, automatically bundling and optimizing them as needed.
+                b.This capability simplifies the development workflow by reducing the manual steps to manage the assets in a project.
+
+
+9. What is .gitignore? What should we add and not add to it?
+--->1.The .gitignore file is a configuration file used by git to specify files and directories that should be ignore when tracking
+    changes in a git repository.
+    2.It allow you to exclude certain file or pattern from being included in a version control system.
+    3.The .gitignore file is used to prevent sensitive or unncessary files from beign committed to the repository
+
+    what we can add to it---> Those packages or dependencies we can regenerate that we can add to .gitignore file
+                              sensitive information add to .gitignore file
+    What we cannot add to it---> Important files which rely on project like source code, api's, configuration files like package.json
+
+
+10.What is the difference between package.json and package-lock.json?   
+---> Package.json:   
+            1.Package.json is used to manage metadata about a project, as well as to define project dependencies, scripts and other configurations.
+            2.Content:
+                a.Project metadata(name, version, license, description, author, etc.)
+                b.List of project dependencies and their versions.
+                c.scripts to run various task(like start, build, test, etc)
+                d.Development tools and other configurations.
+            3.NOTE:While package.json specifies the dependencies and their versions, it doesn't gurantee that these versions will be the same across  different development environment.That where package-lock.json comes in.
+    Package-lock.json:
+            1.Package-lock.json is automatically generated and updated by npm to provide a detailed, version-locked representation of the project's dependency tree
+            2.content:
+                a.The exact version of all installed dependencies and their transitive dependencies.
+
+11.Why should I not modify package-lock.json?
+--->Consistency across environment:     
+            1.Package-lock.json ensure that everyone working on the project installed the exact same version of dependencies. If you modify it, you might introduce inconsistencies leading to different version being installed on different machines.
+    Reproducibilty:
+            1.It help make your project more reproducible.Other developer and development environment can create same dependencies setup, ensuring a consistent and predictable behaviour, which is crucial for collaboration and deployemnt
+            2.Npm and yarn are designed to managae and update package-lock.json changes on package.json.
+        
+12.What is node_moudules? Is it good idea to push it on git?
+---> 1.node_modules is a directory create by Node.js or NPM to store the dependencies that your project relies on. When you run 'npm install' command, npm install theses dependencies in the node_modules directory.
+     2.No, It is not a good idea to push node_modules to git, here are the reasons:
+            1.Size
+            2.Duplication-->Each developer working on the project can run 'npm install'
+            3.Reproducibility.
+
+13.What is the dist folder?
+--->dist stand for 'distribution' commonly used in software development to hold files that are ready to be deployed or distributed to users. It contains the final, optimized and production ready version of your application.
+
+14.What is browserslists 'Read about dif bundlers: vite, webpack, parcel'
+--->browserslist is a configuration file and query tool used in web development to specify which browsers your project should support.
+
+15.What is caret(^) and tilda(~);
+---> caret(^):
+        1.It is a symbole used before where we write version
+        2.It means that whenever a minor update is happen in a package or dependencies it will update it
+    Tilda(~):
+        1.It is same as caret but it update the major update.
 */
+
