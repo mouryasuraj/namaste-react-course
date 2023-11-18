cd34a7df95d4f2909ef474910384a65f74976aed
Suraj <mouryasuraj6266444@gmail.com>
2023-11-18 16:41:40 +0530

    COmpleted half assignment Laying the foundation

---
+++ Assignments/03.03.LayingTheFoundation.js
@@ -0,0 +1,27 @@
+/*
+
+1.What is JSX?
+---->1. JSX is a syntax where we can easily create element
+     2. JSX is like html and xml not html and xml.
+     3. With the help of JSX we can write html like code directly into your  javascript code.
+     4. JSX stand for Javascript XML.
+     5. JSX is a syntax extension for JS
+     6. JSX provide Embedding Expressions within curly braces {}
+     7. JSX is not directly understood by browser. It needs to transformed code into html using tools like babel before being served to the browser.
+     8.In JSx, we use className instead of class in html attribute and closing tag is important.
+     10.Always write attibutes in camelCase in JSX.
+
+2.SuperPower of JSX?
+---> 1.Declarative Syntax
+     2.Component Composition
+     3.Embedding JS Expressions
+     4.Readibilty and Conciseness
+     5.Babel Transformation
+
+3. What is type attribute in script tag?
+--->1.text/javascript --- It is a default
+    2.application/javscript
+    3.application/ecmascript
+    4.module
+
+*/
