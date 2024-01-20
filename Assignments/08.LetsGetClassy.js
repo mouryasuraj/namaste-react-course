6634381f588b9213c59bc0be267e59d5841afd5b
Suraj <mouryasuraj6266444@gmail.com>
2024-01-20 11:41:50 +0530

    COmpleted: Assignment - 08.lets Get Classy

---
+++ Assignments/08.LetsGetClassy.js
@@ -0,0 +1,140 @@
+/*
+
+1.How do you creates nested routes react-router-dom configurtaion?
+---> 
+    1.Install react-router-dom in your project using:
+       -->  "npm i react-router-dom"
+    2.Import createBrowserRouter() from react-router-dom in entry level js file like App.js or index.js:
+        import {createBrowserRouter} from 'react-router-dom';
+    3.Create a variable and assigned createBroweserRouter() to it. 
+       -->  const appRouter = createBrowserRouter()
+    4.Pass an array of objects in createBrowserRouter() as a argument.
+       -->  const appRouter = createBrowserRouter([{},{}])
+    5.In the array of object, we can pass the key of path, element, error(Once, we create error element at the top level of object then it will handle all the error).
+       -->  const appRouter = createBrowserRouter([
+                {
+                    path:'/',
+                    element:'<App />',
+                    errorElement: <Error />
+                }
+                {
+                    path:'/about',
+                    element:'<About />',
+                }
+            ])
+    6.If we want to create nedted router then we need to put children key in the object where we want to create children.
+    7.Children also take array of objects which have path, element, childrem ,errorElement, etc keys.
+       -->  const appRouter = createBrowserRouter([
+                {
+                    path:'/',
+                    element:'<App />',
+                    children:[
+                        {
+                            path:'/',
+                            element:<Home />
+                        },
+                        {
+                            path:'/contact',
+                            element:<Contact />,
+                            children:[
+                                {
+                                    path:'/',
+                                    element:<Person />
+                                }
+                            ]
+                        },
+                        {
+                            path:'/cart',
+                            element:<Cart />
+                        },
+                    ],
+                    errorElement: <Error />
+                }
+                {
+                    path:'/about',
+                    element:'<About />',
+                }
+            ])
+    8.Subsequently, we have pass this appRouter to the RouterProvider component as a prop to show the components according to the path, but first we have to import RouterProvider from reat-router-dom.
+       --> import {RouterProvider} from 'react-router-dom';
+            
+            const rootDom = ReactDOM.createRoot(document.getElementById('root));
+            rootDom.render(<RouterProvider router={appRouter} />)
+    9.All set with configuration of routes and nested router, only you need to create all the component provided in the path.
+
+
+2.Read about createHashRouter and createMemoryRouter from react-router-dom?
+
+3.What is the order of lifecycle methods calls in class based component?
+--> 
+    1.Mounting - There are two phases in Mounting:
+            a.Render Phase:
+                1.When an instance of that class is created then, first constructor(with some inital data or shimmer UI) will be call.
+                2.After constructor render(render that initial data) method will call.
+            b.Commit Phase
+                1.HTML DOM update with initial data.
+                2.Then after dom updated, componentDidMount will be call.
+                3.Data will update with new data during comonentDidMount()
+    2.Updating:
+            a.Render Phase: 
+                1.Render the element with new data.
+            b.Commit Phase:
+                1.HTML DOM update with new Data
+                2.Then after dom updated, componentDidUpdate will be call
+                3.ComponentDidUpdate is used to update every time if props or state changes.
+    3.Unmounting: It will call the methode just before component unmounting.
+
+4.Why do we use componentDidMount()?
+--> 
+    1.It is a part of lifecycle method in class based component.
+    2.It is used to perform some actions or sideEffects like api's calling, after the component has been rendered into the DOM.
+    3.It is called immediately after the component is mounted or inserted into the DOM tree.
+    4.Use of it:
+            1.API's calling
+            2.Subscription to external resource
+            3.Setting timers
+            4.Mnipulationg DOM
+    
+5.Why do we use componentWillMount? Show with example?
+--> 
+    1.It is the last portion of lifecycle in class based component.
+    2.It is used to clear some operation just before unmounted or removed from the DOM.
+    3.Example:
+            1.Suppose, you created a timeInterval in componentDidMount method.
+                componentDIdMount(){
+                    this.timer = setInterval(()=>{
+                        console.log('Interval')
+                    },1000)
+                }
+            2.And, I visited to other component like about, it will to that componet, but still our setInterval is executing after i leave that component and when again visited to that componet then it create another setinterval.
+            3.So, basically if i again and again render that componet it will create setInterval, that the problem, it can may be crash our application at some time.
+            4.To avoid this issue, we use componentWillMount()
+                componentDIdMount(){
+                    this.timer = setInterval(()=>{
+                        console.log('Interval')
+                    },1000)
+                }
+                componentWillMount(){
+                    clearInterval(this.timer);
+                }
+            5.We put our clearInterval in componentWillMount because before unmounting it will call.
+    
+6.why do we use super(props) in constructor?
+--> 
+    1.In the class based component, when you define constructor, it's important to call super(props) as the fist statement in the constructor.
+    2.This is necessary when the component extends another class, like "React.Component".
+    3.The super(props) call is responsible for invoking the parent constructor and initializing the "this" object.
+    4.Without super(props) call, "this" object within the componet would not be properly setup, it would encountered unexpected behaviour.
+    5.When you call super(props), you are passing the props object to the constructor of parent class.
+    6.This allow the parent class to initialize its own properties related to the comopnent's props.
+    7.If you omit props in super() method, then this.props in parent class might be undefined within your component, leading to erros when accessing the props.
+
+7.Why can't we have the callback funciton of useEffect async?
+--> 
+    1.The useEffect hook in React is designed to handle side effects in a declarative way. 
+    2.It allows you to perform actions after the component renders, such as data fetching, subscriptions, or manually interacting with the DOM. 
+    3.While you can use asynchronous functions inside the useEffect callback, there are some important considerations to keep in mind.
+    4.The useEffect callback itself cannot be declared as async because the return value of the callback is expected to be either a cleanup function or undefined. 
+    5.If you make the useEffect callback async and return a Promise, React won't handle it correctly, leading to potential issues.
+
+ */
--- Notes/08.LetsGetClassy.js
+++ Notes/08.LetsGetClassy.js
@@ -18,7 +18,7 @@
         }
     }
 4.Here,if you extends React.Component then react will know that this is a class based component.
-5.render() method is used to return some piece of js code.
+5.render() method is used to return some piece of jsx.
 6.Fucntion component is normal js fucntion which return some piece of jsx and class based component is just a normal class which has render() method which return some piece of jsx.
 7.UserClass extends React.Component, here component is a class which is present in React package. So, userClass is extending Component class, it means that we can use method available in Component class.
 8.When a new instance of class is created, initially constructor is called and fetch props from the component where its instance is created.
@@ -33,11 +33,11 @@
 3.We can create state variable like this:
     constructor(){
         this.state = {
-            coutn:0
+            count:0
         }
     }
-4.state is a reserved keyword in js and state take big object of state variables
-5.In functional based component we have to create separate state variables but behind the scene react collect all the useState variable in big object.
+4.state is a reserved keyword in js and state takes big object of state variables
+5.In functional  component, we have to create separate state variables but behind the scene react collect all the useState variable in big object.
 
 // How to update these state variables in class based components:
 1.Never update state variable directly like this:
@@ -60,7 +60,7 @@
 2.It only called once in a program at the initial time after render() method
 3.The method name itself said component is mounted on the browser.
 4.It is used to make api call, subscription, DOM manipulations, setting up timers
-5.We used to make api calls in componentDidMOunt() because first it renders whatever we have in our code and then componentDidMount calls and make an api call and re-render the component with updated values.
+5.We used to make api calls in componentDidMOunt() because first it renders whatever we have in our code and then componentDidMount calls and make an api call and mount with updated values.
 6.Like useEffect(), but useEffect is used to handle all lifecycle. and in componentDidMount() is only called once initially. to update the the component when something is change, we have to use componentDidUpdate()
 
 
@@ -94,7 +94,7 @@ To clear this confusion see the lifecycle diagram of class based component
 
 1.In the diagram, it shows that lifecycle of class based component is divided into two part:
 1.Render Phase:
-    a.Constructor and Render Method is comes into Render Phase.
+    a.Constructor and Render Method comes into Render Phase.
     c.In our code, first constructor will call and then render will called of parent and then constructor and render will be batched together and perform render phase and then together they perform commit phase.
     Basically, React create children batches to optimize the code. 
     Updating the DOM, is most expensive thing, so that's why react make batches of render phase separate and commit phase separate and run separately
@@ -121,7 +121,7 @@ Commit Phase:
     3.Then, at the end componentDidUpdate() call
 
 // --------Unmounting-------
-    1.It will call before any component unmount or remove from the dom.
+    1.It will call before any component unmountimng or remove from the dom.
     2.componentWillUnmount is a lifecycle method in React class components that gets called just before a component is unmounted or removed from the DOM. It provides an opportunity to perform cleanup tasks, unsubscribe from external services, or clear up resources associated with the component.
 
 Note: 1.Never ever compare useEffect with componentDidMount(). They have some similar properites but both are different from each other.
@@ -142,7 +142,7 @@ Note: 1.Never ever compare useEffect with componentDidMount(). They have some si
 1.Lets take an example, suppose we have setInterval in componentDidMount() and setINterval is set to 1s.
 2.So, what happen in componentDidMount is it only called at the time of initial render but it will run continuouly when changing the component beacuse its not refereshing the page so that's why it it calling again and again.
 3.And when you go to again on that component it will create another setInterval for 1s
-4.Now, here comes a picture of componentWillUnmount(); so we can clear this type of cons in our app;
+4.Now, here comes a picture of componentWillUnmount(); so we can clear this type of issues in our app;
 5.How:
             componentDidMount(){
                 this.timer = setInterval(()=>{
@@ -160,7 +160,7 @@ how:
                 })
 
                 return ()=>{
-                    clear
+                    clearInterval(timer);
                 }
             },[])
  */
